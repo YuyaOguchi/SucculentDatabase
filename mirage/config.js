0a29b16fb2008be53efc27f8dfdc7db0074b35ac
Yuya Oguchi <yuyancal08@gmail.com>
2018-05-25 13:01:06 -0700

    API set up for backend

---
+++ mirage/config.js
@@ -0,0 +1,43 @@
+export default function() {
+  this.namespace = '/api';
+
+  this.get('/succulents', function() {
+    return {
+      data: [{
+        type: 'succulent',
+        id: 'succ1',
+        attributes: {
+          title: 'Succulent1',
+          owner: 'Me',
+          city: 'Succulent City',
+          category: 'Succulent',
+          bedrooms: 1,
+          image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+        }
+      }, {
+        type: 'succulent',
+        id: 'succ2',
+        attributes: {
+          title: 'Succulent2',
+          owner: 'Me',
+          city: 'Succulent City2',
+          category: 'Succulent',
+          bedrooms: 2,
+          image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+        }
+      }, {
+        type: 'succulent',
+        id: 'succ3',
+        attributes: {
+          title: 'Succulent3',
+          owner: 'Me',
+          city: 'Succulent city3',
+          category: 'Succulent',
+          bedrooms: 3,
+          image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+        }
+      }]
+    };
+  });
+}
+
